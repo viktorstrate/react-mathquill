ee8f3dca490c134213ced56e387217104e9e6c33
David Li <dawei.li@gmx.net>
2019-07-31 17:39:53 +0200

    using newer mathquill version

--- src/mathquill-loader.js
+++ src/mathquill-loader.js
@@ -1,2 +1,2 @@
-export const MathQuill = require('exports-loader?window.MathQuill!imports-loader?window.jQuery=jquery!mathquill/build/mathquill.js')
+export const MathQuill = require('exports-loader?window.MathQuill!imports-loader?window.jQuery=jquery!@edtr-io/mathquill/build/mathquill.js')
 export const MQ = MathQuill.getInterface(2)
