a0d9875ebddb46776d5057e75703acfd31631c1c
Nesh Shanth <nshanth@tvo.org>
2022-08-11 15:05:48 -0400

    Forked library with changes

--- src/EditableMathField.js
+++ src/EditableMathField.js
@@ -1,69 +1,86 @@
-import React, { useEffect, useRef } from 'react'
+import React, {
+  useEffect,
+  useRef,
+  useImperativeHandle,
+  forwardRef,
+} from 'react'
 import PropTypes from 'prop-types'
 import MathQuill from './mathquill-loader'
 
-const EditableMathField = ({
-  latex,
-  onChange,
-  config,
-  mathquillDidMount,
-  ...otherProps
-}) => {
-  // MathQuill fire 2 edit events on startup.
-  const ignoreEditEvents = useRef(2)
-  const mathField = useRef(null)
-  const wrapperElement = useRef(null);
+const EditableMathField =
+  forwardRef <
+  HTMLInputElement >
+  (({ latex, onChange, config, mathquillDidMount, ...otherProps }, ref) => {
+    // MathQuill fire 2 edit events on startup.
+    const ignoreEditEvents = useRef(2)
+    const mathField = useRef(null)
+    const wrapperElement = useRef(null)
 
-  // This is required to prevent state closure over the onChange function
-  const onChangeRef = useRef(onChange);
-  useEffect(() => {
-    onChangeRef.current = onChange;
-  }, [onChange])
+    useImperativeHandle(ref, () => {
+      const mqField = mathField.current
 
-  // Setup MathQuill on the wrapperElement
-  useEffect(() => {
-    if (!wrapperElement.current) return
-
-    let combinedConfig = {
-      restrictMismatchedBrackets: true,
-      handlers: {},
-    }
-
-    if (config) {
-      combinedConfig = {
-        ...combinedConfig,
-        ...config,
+      //Expose MathQuill functions
+      return {
+        getLatex: () => mqField.latex(),
+        useCmd: (value) => mqField.cmd(value),
+        useWrite: (value) => mqField.write(value),
+        setFocus: () => mqField.focus(),
+        onBlur: () => mqField.blur(),
+        useSelect: () => mqField.select(),
+        useClearSelection: () => mqField.clearSelection(),
+        setConfig: (value) => mqField.config(value),
       }
-    }
+    })
+    // This is required to prevent state closure over the onChange function
+    const onChangeRef = useRef(onChange)
+    useEffect(() => {
+      onChangeRef.current = onChange
+    }, [onChange])
 
-    const configEditHandler = combinedConfig.handlers.edit
-    combinedConfig.handlers.edit = (mathField) => {
-      if (configEditHandler) configEditHandler()
+    // Setup MathQuill on the wrapperElement
+    useEffect(() => {
+      if (!wrapperElement.current) return
 
-      if (ignoreEditEvents.current > 0) {
-        ignoreEditEvents.current -= 1
-      } else {
-        if (onChangeRef.current) onChangeRef.current(mathField)
+      let combinedConfig = {
+        restrictMismatchedBrackets: true,
+        handlers: {},
       }
-    }
 
-    mathField.current = MathQuill.MathField(
-      wrapperElement.current,
-      combinedConfig
-    )
-    mathField.current.latex(latex || '')
+      if (config) {
+        combinedConfig = {
+          ...combinedConfig,
+          ...config,
+        }
+      }
 
-    if (mathquillDidMount) mathquillDidMount(mathField.current)
-  }, [wrapperElement])
+      const configEditHandler = combinedConfig.handlers.edit
+      combinedConfig.handlers.edit = (mathField) => {
+        if (configEditHandler) configEditHandler()
 
-  useEffect(() => {
-    if (mathField.current && mathField.current.latex() !== latex) {
-      mathField.current.latex(latex)
-    }
-  }, [latex])
+        if (ignoreEditEvents.current > 0) {
+          ignoreEditEvents.current -= 1
+        } else {
+          if (onChangeRef.current) onChangeRef.current(mathField)
+        }
+      }
 
-  return <span {...otherProps} ref={wrapperElement} />
-}
+      mathField.current = MathQuill.MathField(
+        wrapperElement.current,
+        combinedConfig
+      )
+      mathField.current.latex(latex || '')
+
+      if (mathquillDidMount) mathquillDidMount(mathField.current)
+    }, [wrapperElement])
+
+    useEffect(() => {
+      if (mathField.current && mathField.current.latex() !== latex) {
+        mathField.current.latex(latex)
+      }
+    }, [latex])
+
+    return <span {...otherProps} ref={wrapperElement} />
+  })
 
 EditableMathField.propTypes = {
   latex: PropTypes.string,
